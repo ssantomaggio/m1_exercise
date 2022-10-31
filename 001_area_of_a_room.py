37750a391d450d7ce54da611992c62e03bcf0d24
ssantomaggio <stefano.santomaggio02@gmail.com>
2022-10-31 13:02:51 +0100

    test

--- 001_area_of_a_room.py
+++ 001_area_of_a_room.py
@@ -9,4 +9,4 @@
 width=float(input("insert the width of the room: "))
 lenght=float(input("insert the lenght of the room: "))
 Area=width*lenght
-print("The area of the room is",Area)
+print("The area of the room is:",Area)
